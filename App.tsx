1d2ceccbd938c7abc995348ff6b748af74b37796
Alireza Alikhani <Alirezaalikhani421@gmail.com>
2025-12-19 16:16:25 +0330

    background nitification working

--- App.tsx
+++ App.tsx
@@ -1,13 +1,41 @@
 import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
+import { Button, View, NativeModules, ScrollView } from 'react-native';
 
 import './global.css';
 
+// 🔹 Get native module
+const { StepCounter } = NativeModules;
+
 export default function App() {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+    <ScrollView>
+    <View style={{ flex: 1, marginTop: 60 }}>
+      
+      <ScreenContent title="Home" path="App.tsx" />
+
+      <View style={{ marginTop: 40, paddingHorizontal: 20 }}>
+        <Button
+          title="Start Background Step Service"
+          onPress={() => {
+            console.log('Starting service');
+            StepCounter.startService();
+          }}
+        />
+
+        <View style={{ height: 20 }} />
+
+        <Button
+          title="Stop Background Step Service"
+          onPress={() => {
+            console.log('Stopping service');
+            StepCounter.stopService();
+          }}
+        />
+      </View>
+
       <StatusBar style="auto" />
-    </>
+    </View>
+    </ScrollView>
   );
 }
